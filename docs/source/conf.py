d196b3339c516dc4fa4bd69f9f9a9ddbf44e9081
Collin Cappelle <collin.cappelle@gmail.com>
2019-01-31 11:29:57 -0500

    Possible Theme fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -43,6 +43,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
+    'sphinx.ext.githubpages',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
     'sphinx.ext.autosummary',
